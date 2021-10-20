64b7b26410160e85db6454d14847e6ef496710aa
WR0903 <918981878@qq.com>
2021-10-20 22:56:50 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module registryservice
+
+go 1.14
+
+require github.com/WR0903/distributedSDK v1.0.3
+
+replace github.com/WR0903/distributedSDK => github.com/WR0903/distributedSDK v1.0.3
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/WR0903/distributedSDK/registry"
+)
+
+func main() {
+	registry.SetupRegistryService()
+	http.Handle("/services", &registry.RegistryService{})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var srv http.Server
+	srv.Addr = registry.ServerPort
+
+	go func() {
+		log.Println(srv.ListenAndServe())
+		cancel()
+	}()
+
+	go func() {
+		fmt.Println("Registry service started. Press any key to stop.")
+		var s string
+		fmt.Scanln(&s)
+		srv.Shutdown(ctx)
+		cancel()
+	}()
+
+	<-ctx.Done()
+	fmt.Println("Shutting down registry service")
+}
